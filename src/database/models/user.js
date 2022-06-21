9b0136aeca66a372441cbbf96c2383c473dd7cdf
Victor Hugo <victorhsmsantos@gmail.com>
2022-06-21 18:56:12 -0300

    Adicionado id ao model de user

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -2,6 +2,11 @@ const UserSchema = (sequelize, DataTypes) => {
   const UserTable = sequelize.define(
     "User",
     {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
       displayName: DataTypes.STRING,
       email: {
         type: DataTypes.STRING,
