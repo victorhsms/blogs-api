308c2dc8352970987216cc1ff1e150d64905595c
Victor Hugo <victorhsmsantos@gmail.com>
2022-06-21 18:50:30 -0300

    Feita o movel de user

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,26 +1,21 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+const UserSchema = (sequelize, DataTypes) => {
+  const UserTable = sequelize.define(
+    "User",
+    {
+      displayName: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
     }
-  };
-  User.init({
-    displayName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    image: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
+  );
+
+  return UserTable;
 };
+
+module.exports = UserSchema;
