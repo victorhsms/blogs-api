8e5974dfe5c50190acf37e54119859f7a8eee4db
Victor Hugo <victorhsmsantos@gmail.com>
2022-06-23 12:36:25 -0300

    Criado model de Category

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -22,3 +22,25 @@ module.exports = (sequelize, DataTypes) => {
   );
   return Category;
 };
+
+
+const CategorySchema = (sequelize, DataTypes) => {
+  const CategoryTable = sequelize.define(
+    "Category",
+    {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+    }
+  );
+
+  return CategoryTable;
+};
+
+module.exports = CategorySchema;
