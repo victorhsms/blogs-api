2e12457a41efc6e780611fe5e7aaf9a3681fffa5
Victor Hugo <victorhsmsantos@gmail.com>
2022-06-23 13:32:42 -0300

    Deletado modelo POO das models

--- src/database/models/blogPost.js
+++ src/database/models/blogPost.js
@@ -1,29 +1,3 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class blogPost extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  blogPost.init({
-    title: DataTypes.STRING,
-    content: DataTypes.STRING,
-    userId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'blogPost',
-  });
-  return blogPost;
-};
-
 const BlogPostSchema = (sequelize, DataTypes) => {
   const BlogPostTable = sequelize.define(
     "BlogPost",
